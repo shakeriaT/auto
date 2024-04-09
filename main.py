b0803d18420db1bb5ba162cce7e23eb3aa955711
shakeriaT <155984698+shakeriaT@users.noreply.github.com>
2024-04-09 18:38:55 +0000

    vol 4

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 userinput = input("Please Enter the choice number from the menu: ")
 
-allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'RAM 1500']
+allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Rivian R1T', 'RAM 1500']
 
 print("********************************")
-print("AutoCountry Vehicle Finder v0.3")
+print("AutoCountry Vehicle Finder v0.4")
 print("********************************")
 print("Please Enter the following number below from the following menu:")
 
 print("1. PRINT all Authorized Vehicles")
 print("2. SEARCH for Authorized Vehicle")
 print("3. ADD Authorized Vehicle")
-print("4. Exit")
+print("4.DELETE Authorized Vehicle")
+print("5. Exit")
 print("********************************")
 
 if userinput == '1':
@@ -31,7 +32,16 @@ elif userinput == '3':
       print(f"{new_vehicle} has been added to the authorized vehicles list.")
 
 elif userinput == '4':
-      print("Exiting...")
+    remove_vehicle = input("Please enter full vehicle name you would like to remove:")
+    allowedVehiclesList.remove(remove_vehicle)
+    print(f"{remove_vehicle} Are you sure you want to remove '{remove_vehicle}' from the Authorized Vehicles List?")
+    confirmation = input(f"Are you sure you want to remove '{remove_vehicle}' from the Authorized Vehicles List?:")
+    if confirmation.lower() == 'yes':
+        print(f"You have REMOVED '{remove_vehicle}' as an authorized vehicle") 
+  
+elif userinput == '5':
+  print("Exiting...")
+  
 
 else:
       print("Invalid choice. Please select a valid option.")
