cc2f786251184cdd41128c4f6f9fba1aa2a5c141
shakeriaT <155984698+shakeriaT@users.noreply.github.com>
2024-04-26 13:59:26 +0000

    note sure

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
-userinput = input("Please Enter the choice number from the menu: ")
+#Repo pattern = CRUD
 
-allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Rivian R1T', 'RAM 1500']
+#CREATE
 
-print("********************************")
-print("AutoCountry Vehicle Finder v0.4")
-print("********************************")
-print("Please Enter the following number below from the following menu:")
+#READ
 
-print("1. PRINT all Authorized Vehicles")
-print("2. SEARCH for Authorized Vehicle")
-print("3. ADD Authorized Vehicle")
-print("4.DELETE Authorized Vehicle")
-print("5. ")
-print("********************************")
+#UPDATE
 
-if userinput == '1':
-      print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
-      for vehicle in allowedVehiclesList:
-          print(vehicle)
+#DELETE
+import os
+import pickle as list
 
-elif userinput == '2':
-      search_vehicle = input("Please Enter the full Vehicle name you would like to search:")
-      if search_vehicle in allowedVehiclesList:
-          print(f"{search_vehicle} is an authorized vehicle.")
-      else:
-          print(f"{search_vehicle} is not an authorized vehicle.")
+db = open("main.py/my_list", "r")
+response = db.read()
+print(response)
 
-elif userinput == '3':
-      new_vehicle = input("Please Enter the full Vehicle name you would like to add:")
-      allowedVehiclesList.append(new_vehicle)
-      print(f"{new_vehicle} has been added to the authorized vehicles list.")
+# imports
+import list
 
-elif userinput == '4':
-    remove_vehicle = input("Please enter full vehicle name you would like to remove:")
-    allowedVehiclesList.remove(remove_vehicle)
-    print(f"{remove_vehicle} Are you sure you want to remove '{remove_vehicle}' from the Authorized Vehicles List?")
-    confirmation = input(f"Are you sure you want to remove '{remove_vehicle}' from the Authorized Vehicles List?:")
-    if confirmation.lower() == 'yes':
-        print(f"You have REMOVED '{remove_vehicle}' as an authorized vehicle") 
-  
-elif userinput == '5':
-  print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-  
+# CRUD: Create function
+def create(obj):
+    if not os.path.exists('main.py/my_list'):
+        open("main.py/my_list", mode="x")
+    with open("main.py/my_list", mode="w") as db:
+        list.dump(obj, db)
 
-else:
-      print("Invalid choice. Please select a valid option.")
+# CRUD: Read function
+def read():
+    pass
+
+def update(obj):
+    with open("main.py/my_list", mode="a") as db:
+        list.dump(obj, db)
+
+events = read()
+create(events)
